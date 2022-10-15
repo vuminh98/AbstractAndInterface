482b7d9b874ae42e11739a9b24c513545bfeb5d2
vuminh98 <vuminhtran27@gmail.com>
2022-10-15 09:46:27 +0700

    m

---
+++ bai1/Animal.java
@@ -0,0 +1,5 @@
+package bai1;
+
+public abstract class Animal {
+    public abstract String makeSound();
+}
---
+++ bai1/Tiger.java
@@ -0,0 +1,9 @@
+package bai1;
+
+public class Tiger extends Animal {
+    @Override
+    public String makeSound() {
+        return "Tiger: roarrrrr!";
+    }
+
+}
